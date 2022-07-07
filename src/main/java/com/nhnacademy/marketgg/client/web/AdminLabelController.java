4533f336c9ba9757238988a4bff9ed36d178e3bd
psw4887 <psw4887@naver.com>
2022-07-08 00:28:11 +0900

    Style: 라벨 관리 코드 컨벤션 적용 #2

--- src/main/java/com/nhnacademy/marketgg/client/web/AdminLabelController.java
+++ src/main/java/com/nhnacademy/marketgg/client/web/AdminLabelController.java
@@ -5,7 +5,11 @@ import com.nhnacademy.marketgg.client.dto.LabelRegisterRequest;
 import com.nhnacademy.marketgg.client.service.LabelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
--- src/test/java/com/nhnacademy/marketgg/client/web/AdminLabelControllerTest.java
+++ src/test/java/com/nhnacademy/marketgg/client/web/AdminLabelControllerTest.java
@@ -5,7 +5,8 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
