93cac260acec4e41de32975eae17bae19a5aacb1
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-07-07 13:34:09 +0900

    Client setting up development environment
    - Add RestTemplate Bean
    - Add gateWayIp Bean
    - Add Default Package

---
+++ src/main/java/com/nhnacademy/marketgg/client/config/WebConfiguration.java
@@ -0,0 +1,25 @@
+package com.nhnacademy.marketgg.client.config;
+
+import java.time.Duration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurationSupport {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setReadTimeout(Duration.ofSeconds(10L))
+                .setConnectTimeout(Duration.ofSeconds(5L))
+                .build();
+    }
+
+    @Bean
+    public String gatewayIp() {
+        return "http://localhost:6060";
+    }
+}
