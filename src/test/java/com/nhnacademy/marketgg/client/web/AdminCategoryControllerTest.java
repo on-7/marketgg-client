f3fef5d49bead1e838926ea15f4f88729f62870d
Moomin <moominK228@gmail.com>
2022-07-11 13:09:17 +0900

    Test: 카테고리 전체 조회 테스트 코드 구현 #7

---
+++ src/test/java/com/nhnacademy/marketgg/client/web/AdminCategoryControllerTest.java
@@ -0,0 +1,40 @@
+package com.nhnacademy.marketgg.client.web;
+
+import com.nhnacademy.marketgg.client.dto.response.CategoryRetrieveResponse;
+import com.nhnacademy.marketgg.client.service.CategoryService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(AdminCategoryController.class)
+public class AdminCategoryControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    CategoryService categoryService;
+
+    @Test
+    @DisplayName("라벨 전체 목록 조회")
+    void testRetrieveLabels() throws Exception {
+        when(categoryService.retrieveCategories()).thenReturn(List.of(new CategoryRetrieveResponse()));
+
+        mockMvc.perform(get("/admin/v1/categories/index"))
+               .andExpect(status().isOk())
+               .andExpect(view().name("/categories/index"));
+    }
+
+}
