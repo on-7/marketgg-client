2dc88a37487aeaaf089bc9987f2f27e221a414b3
coalong <minay0904@gmail.com>
2022-07-12 17:42:31 +0900

    Feat: Toast Editor API 로 상품 상세 설명 등록 #36

---
+++ src/main/resources/static/js/toast.js
@@ -0,0 +1,16 @@
+const Toast = toastui.Editor;
+
+const editor = new Toast({
+    el: document.querySelector('#editor'),
+    height: 'auto',
+    initialEditType: 'wysiwyg',
+    previewStyle: 'vertical',
+    placeholder: '내용을 입력해주세요.'
+});
+
+const submitButton = document.getElementById('btn-submit');
+submitButton.addEventListener('mouseover', (event) => {
+    const description = editor.getHTML();
+    console.log(description);
+    document.getElementById('toast-content').value = description;
+})
