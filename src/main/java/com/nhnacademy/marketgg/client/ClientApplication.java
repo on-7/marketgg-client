64a70ac92c711719da443ab79d33f493705aea2c
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-07-13 11:34:38 +0900

    Feat: AOP 설정 완료
    - Adapter Method 실행 체크

--- src/main/java/com/nhnacademy/marketgg/client/ClientApplication.java
+++ src/main/java/com/nhnacademy/marketgg/client/ClientApplication.java
@@ -2,7 +2,9 @@ package com.nhnacademy.marketgg.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class ClientApplication {
 
