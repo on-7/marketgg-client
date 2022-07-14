279bf0b31b6b9f0ae4f772e818b013ad87b87e71
psw4887 <psw4887@naver.com>
2022-07-14 22:48:14 +0900

    Docs: Service, Controller JavaDoc 작성 완료 #53

--- src/main/java/com/nhnacademy/marketgg/client/service/MemberService.java
+++ src/main/java/com/nhnacademy/marketgg/client/service/MemberService.java
@@ -1,11 +1,35 @@
 package com.nhnacademy.marketgg.client.service;
 
+/**
+ * 회원 서비스 입니다.
+ *
+ * @version 1.0.0
+ */
 public interface MemberService {
 
+    /**
+     * 지정한 회원의 GG Pass 갱신일시와 현재 일시를 비교하는 메소드입니다.
+     *
+     * @param id - 갱신일시를 확인할 회원의 식별번호입니다.
+     * @return 회원의 갱신일시가 현재 시간보다 적다면 true, 많다면 false 를 반환합니다.
+     * @since 1.0.0
+     */
     Boolean checkPassUpdatedAt(final Long id);
 
+    /**
+     * 지정한 회원의 GG Pass 구독처리를 위한 메소드입니다.
+     *
+     * @param id - GG Pass 구독을 진행 할 회원의 식별번호입니다.
+     * @since 1.0.0
+     */
     void subscribePass(final Long id);
 
+    /**
+     * 지정한 회원의 GG Pass 구독해지처리를 위한 메소드입니다.
+     *
+     * @param id - GG Pass 구독해지를 진행 할 회원의 식별번호입니다.
+     * @since 1.0.0
+     */
     void withdrawPass(final Long id);
 
 }
--- src/main/java/com/nhnacademy/marketgg/client/web/MemberController.java
+++ src/main/java/com/nhnacademy/marketgg/client/web/MemberController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 회원관리에 관련된 Controller 입니다.
+ *
+ * @version 1.0.0
+ */
 @Controller
 @RequestMapping("/shop/v1/members")
 @RequiredArgsConstructor
@@ -17,6 +22,13 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    /**
+     * 선택한 회원의 GG 패스 화면으로 이동합니다.
+     *
+     * @param memberId - GG 패스 페이지를 조회할 회원의 식별번호입니다.
+     * @return 회원의 식별번호를 담고, GG 패스의 index 페이지로 이동합니다.
+     * @since 1.0.0
+     */
     @GetMapping("/{memberId}/ggpass")
     public ModelAndView index(@PathVariable final Long memberId) {
         ModelAndView mav = new ModelAndView("/ggpass/index");
@@ -25,6 +37,14 @@ public class MemberController {
         return mav;
     }
 
+    /**
+     * 선택한 회원을 GG 패스에 구독시키는 Mapping 을 지원합니다.
+     *
+     * @param memberId - GG 패스를 구독할 회원의 식별번호입니다.
+     * @return 갱신일자가 지금 시간보다 늦을 시, ERROR 메세지와 함께 다시 Index 페이지로 이동합니다.
+     * 갱신일자가 지금 시간보다 빠를 시, 구독하는 메소드를 실행한 후, 다시 Index 페이지로 이동합니다.
+     * @since 1.0.0
+     */
     @PostMapping("/{memberId}/ggpass/subscribe")
     public ModelAndView subscribePass(@PathVariable final Long memberId) {
         if (Boolean.TRUE.equals(memberService.checkPassUpdatedAt(memberId))) {
@@ -38,7 +58,13 @@ public class MemberController {
         return new ModelAndView("redirect:/shop/v1/members/" + memberId + "/ggpass");
     }
 
-
+    /**
+     * 선택한 회원을 GG 패스에 구독해지시키는 Mapping 을 지원합니다.
+     *
+     * @param memberId - GG 패스를 구독해지할 회원의 식별번호입니다.
+     * @return GG 패스 구독을 해지하는 메소드 실행 후, 다시 Index 페이지로 이동합니다.
+     * @since 1.0.0
+     */
     @PostMapping("/{memberId}/ggpass/withdraw")
     public ModelAndView withdrawPass(@PathVariable final Long memberId) {
         memberService.withdrawPass(memberId);
