2106b12d8ea65881bf384407fea3bfb5c3ad0137
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-07-15 11:39:54 +0900

    Test: Message 객체 수정

--- src/main/java/com/nhnacademy/marketgg/client/dto/Message.java
+++ src/main/java/com/nhnacademy/marketgg/client/dto/Message.java
@@ -3,8 +3,8 @@ package com.nhnacademy.marketgg.client.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
 @NoArgsConstructor
+@Getter
 public class Message {
 
     private String error;
