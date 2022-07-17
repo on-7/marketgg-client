a814726a968207538858cb32b69a2f16ce88507b
psw4887 <psw4887@naver.com>
2022-07-17 23:42:57 +0900

    Docs: DibAjaxController JavaDoc 작성완료 #63

--- src/main/java/com/nhnacademy/marketgg/client/web/DibAjaxController.java
+++ src/main/java/com/nhnacademy/marketgg/client/web/DibAjaxController.java
@@ -15,6 +15,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * 찜의 비동기 처리를 위한 Controller 입니다.
+ *
+ * @version 1.0.0
+ */
 @Controller
 @RequestMapping("/dibs")
 @RequiredArgsConstructor
@@ -23,11 +28,11 @@ public class DibAjaxController {
     private final DibService dibService;
 
     /**
-     * 지정한 상품을 지정한 회원의 찜 목록에 추가하는 Mapping 을 지원합니다.
+     * 지정한 상품을 지정한 회원의 찜 목록에 존재하는지 판단하는 Mapping 을 지원합니다.
      *
      * @param memberId  - 지정한 회원의 식별변호입니다.
      * @param productId - 지정한 상품의 식별번호입니다.
-     * @return 다시 상품 조회 페이지로 이동합니다.
+     * @return 찜 목록에 상품이 존재하면 1, 없으면 0을 반환합니다.
      * @since 1.0.0
      */
     @PostMapping(produces = "application/json")
@@ -48,6 +53,14 @@ public class DibAjaxController {
         return map;
     }
 
+    /**
+     * 지정한 사용자의 찜 목록에 해당 상품을 추가하는 Mapping 을 지원합니다.
+     *
+     * @param memberId  - 지정한 회원의 식별변호입니다.
+     * @param productId - 지정한 상품의 식별번호입니다.
+     * @return 찜 목록에 해당 상품을 추가하는 메소드 실행 후 정상적인 처리가 됐다는 1을 반환합니다.
+     * @since 1.0.0
+     */
     @GetMapping("/insert/{memberId}/{productId}")
     @ResponseBody
     public int dibInsert(@PathVariable final Long memberId, @PathVariable final Long productId) {
@@ -57,6 +70,14 @@ public class DibAjaxController {
         return 1;
     }
 
+    /**
+     * 지정한 사용자의 찜 목록에서 해당 상품을 제거하는 Mapping 을 지원합니다.
+     *
+     * @param memberId  - 지정한 회원의 식별변호입니다.
+     * @param productId - 지정한 상품의 식별번호입니다.
+     * @return 찜 목록에 해당 상품을 제거하는 메소드 실행 후 정상적인 처리가 됐다는 1을 반환합니다.
+     * @since 1.0.0
+     */
     @GetMapping("/delete/{memberId}/{productId}")
     @ResponseBody
     public int dibDelete(@PathVariable final Long memberId, @PathVariable final Long productId) {
