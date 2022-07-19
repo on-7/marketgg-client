7e64416d30e9508a9d43a9532fe984a0379fe2c2
Moomin <moominK228@gmail.com>
2022-07-19 17:30:45 +0900

    Docs: 쿠폰 관리 JavaDoc 작성 #97
    - Controller, Service, Repository

--- src/main/java/com/nhnacademy/marketgg/client/repository/CouponRepository.java
+++ src/main/java/com/nhnacademy/marketgg/client/repository/CouponRepository.java
@@ -6,16 +6,58 @@ import com.nhnacademy.marketgg.client.dto.response.CouponRetrieveResponse;
 
 import java.util.List;
 
+/**
+ * 쿠폰 레포지토리입니다.
+ *
+ * @version 1.0.0
+ */
 public interface CouponRepository {
 
+    /**
+     * 쿠폰을 등록하는 메소드입니다.
+     *
+     * @param couponRequest - 쿠폰을 등록하기 위한 정보를 담은 객체입니다.
+     * @throws JsonProcessingException - Json 컨텐츠를 처리할 때 발생하는 모든 문제에 대한 예외처리입니다.
+     *
+     * @since 1.0.0
+     */
     void createCoupon(final CouponRequest couponRequest) throws JsonProcessingException;
 
+    /**
+     * 쿠폰 단건 조회를 실행하는 메소드입니다.
+     *
+     * @param couponId - 단건 조회를 할 쿠폰 식별번호입니다.
+     * @return 쿠폰 단건 조회 결과 정보를 담은 객체를 반환합니다.
+     *
+     * @since 1.0.0
+     */
     CouponRetrieveResponse retrieveCoupon(Long couponId);
 
+    /**
+     * 쿠폰 전체 목록 조회를 실행하는 메소드입니다.
+     *
+     * @return 쿠폰 전체 목록 조회 결과 정보를 담은 객체 List 를 반환합니다.
+     *
+     * @since 1.0.0
+     */
     List<CouponRetrieveResponse> retrieveCoupons();
 
+    /**
+     * 쿠폰을 수정하는 메소드입니다.
+     *
+     * @param couponId - 수정할 쿠폰 식별번호입니다.
+     * @param couponRequest - 쿠폰을 수정하기 위한 정보를 담은 객체입니다.
+     * @throws JsonProcessingException - Json 컨텐츠를 처리할 때 발생하는 모든 문제에 대한 예외처리입니다.
+     *
+     * @since 1.0.0
+     */
     void updateCoupon(final Long couponId, final CouponRequest couponRequest) throws JsonProcessingException;
 
+    /**
+     * 쿠폰을 삭제하는 메소드입니다.
+     *
+     * @param couponId - 삭제할 쿠폰 식별번호입니다.
+     */
     void deleteCoupon(Long couponId);
 
 }
--- src/main/java/com/nhnacademy/marketgg/client/web/AdminCouponController.java
+++ src/main/java/com/nhnacademy/marketgg/client/web/AdminCouponController.java
@@ -17,6 +17,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+/**
+ * 쿠폰 관련된 Controller 입니다.
+ *
+ * @since 1.0.0
+ */
 @Controller
 @RequestMapping("/shop/v1/admin/coupons")
 @RequiredArgsConstructor
@@ -24,11 +29,27 @@ public class AdminCouponController {
 
     private final CouponService couponService;
 
+    /**
+     * 쿠폰 등록 페이지로 이동하는 메소드입니다.
+     *
+     * @return 쿠폰 등록 페이지로 이동합니다.
+     *
+     * @since 1.0.0
+     */
     @GetMapping("/create")
     public ModelAndView doCreateCoupon() {
         return new ModelAndView("/coupons/create-form");
     }
 
+    /**
+     * 쿠폰을 등록하고 쿠폰 index 페이지로 이동하는 메소드입니다.
+     *
+     * @param couponRequest - 쿠폰 등록을 위한 정보를 담은 객체입니다.
+     * @return - 쿠폰 index 페이지로 REDIRECT 합니다.
+     * @throws JsonProcessingException - Json 컨텐츠를 처리할 때 발생하는 모든 문제에 대한 예외처리입니다.
+     *
+     * @since 1.0.0
+     */
     @PostMapping
     public ModelAndView createCoupon(@ModelAttribute final CouponRequest couponRequest) throws JsonProcessingException {
         couponService.createCoupon(couponRequest);
@@ -36,6 +57,12 @@ public class AdminCouponController {
         return new ModelAndView("redirect:/shop/v1/admin/coupons/index");
     }
 
+    /**
+     * 쿠폰 전체 목록을 조회한 정보를 담아 index 페이지로 이동하는 메소드입니다.
+     *
+     * @return 쿠폰 전체 목록 정보와 함께 쿠폰 index 페이지로 이동합니다.
+     * @since 1.0.0
+     */
     @GetMapping("/index")
     public ModelAndView retrieveCoupons() {
         List<CouponRetrieveResponse> responses = couponService.retrieveCoupons();
@@ -46,6 +73,14 @@ public class AdminCouponController {
         return mav;
     }
 
+    /**
+     * 쿠폰 수정을 위해 해당 쿠폰을 조회한 정보를 담고 쿠폰 수정 페이지로 이동하는 메소드입니다.
+     *
+     * @param couponId - 수정을 하려는 쿠폰 식별번호입니다.
+     * @return 수정하려는 쿠폰 정보를 담고 쿠폰 수정 페이지로 이동합니다.
+     *
+     * @since 1.0.0
+     */
     @GetMapping("/update/{couponId}")
     public ModelAndView doUpdateCoupon(@PathVariable final Long couponId) {
         ModelAndView mav = new ModelAndView("/coupons/update-form");
@@ -56,6 +91,15 @@ public class AdminCouponController {
         return mav;
     }
 
+    /**
+     * 쿠폰을 수정하고 쿠폰 index 페이지로 이동하는 메소드입니다.
+     * @param couponId - 수정하려는 쿠폰 식별번호입니다.
+     * @param couponRequest - 수정할 내용을 담고 있는 객체입니다.
+     * @return 쿠폰 index 페이지로 REDIRECT 합니다.
+     * @throws JsonProcessingException - Json 컨텐츠를 처리할 때 발생하는 모든 문제에 대한 예외처리입니다.
+     *
+     * @since 1.0.0
+     */
     @PutMapping("/{couponId}")
     public ModelAndView updateCoupon(@PathVariable final Long couponId,
                                      @ModelAttribute final CouponRequest couponRequest) throws JsonProcessingException {
@@ -64,6 +108,14 @@ public class AdminCouponController {
         return new ModelAndView("redirect:/shop/v1/admin/coupons/index");
     }
 
+    /**
+     * 쿠폰을 삭제하고 쿠폰 index 페이지로 이동하는 메소드입니다.
+     *
+     * @param couponId - 삭제하려는 쿠폰 식별번호입니다.
+     * @return 쿠폰 index 페이지로 REDIRECT 합니다.
+     *
+     * @since 1.0.0
+     */
     @DeleteMapping("/{couponId}")
     public ModelAndView deleteCoupon(@PathVariable final Long couponId) {
         couponService.deleteCoupon(couponId);
