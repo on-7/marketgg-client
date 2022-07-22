fe5042f0935ef5aca2fbdcdd5d7a36abac23af9e
Dongyeol <eastheat10@gmail.com>
2022-07-22 11:41:30 +0900

    Style: 오탈자 수정 #76

--- src/main/java/com/nhnacademy/marketgg/client/exception/LogoutException.java
+++ src/main/java/com/nhnacademy/marketgg/client/exception/LogoutException.java
@@ -7,6 +7,6 @@ public class LogoutException extends RuntimeException {
     private static final String LOGOUT_FAIL_MESSAGE = "로그아웃 실패!";
 
     public LogoutException(HttpStatus httpStatus) {
-        super("[" + httpStatus + " " + LOGOUT_FAIL_MESSAGE);
+        super("[" + httpStatus + "] " + LOGOUT_FAIL_MESSAGE);
     }
 }
