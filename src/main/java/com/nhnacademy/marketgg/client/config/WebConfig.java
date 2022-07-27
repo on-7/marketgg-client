8624a405f15590042465d759a71ddbcbc8ee5ee6
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-07-27 10:46:57 +0900

    Feat: Security Fix #84

--- src/main/java/com/nhnacademy/marketgg/client/config/WebConfig.java
+++ src/main/java/com/nhnacademy/marketgg/client/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.marketgg.client.config;
 
+import java.security.KeyStore;
 import java.time.Duration;
+import javax.net.ssl.SSLContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +11,35 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.time.Duration;
+import java.util.Optional;
+import javax.net.ssl.SSLContext;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * Web Configuration 을 설정할 수 있습니다.
  *
  * @version 1.0.0
  */
+@Slf4j
 @Configuration
 public class WebConfig {
 
@@ -61,6 +87,60 @@ public class WebConfig {
         commonsMultipartResolver.setDefaultEncoding("utf-8");
         commonsMultipartResolver.setMaxUploadSize(50000000);
         return commonsMultipartResolver;
+    }
+
+    @Bean
+    CloseableHttpClient httpClient(final @Value("${gg.keystore.type}") String keystoreType,
+                                   final @Value("${gg.keystore.path}") String keystorePath,
+                                   final @Value("${gg.keystore.password}") String keystorePassword,
+                                   final @Value("${gg.protocol}") String protocol) {
+
+        SSLContext sslContext;
+
+        try {
+
+            KeyStore clientStore = KeyStore.getInstance(keystoreType);
+            Resource resource = new ClassPathResource(keystorePath);
+            clientStore.load(resource.getInputStream(), keystorePassword.toCharArray());
+
+            sslContext = SSLContexts.custom()
+                                    .setProtocol(protocol)
+                                    .loadKeyMaterial(clientStore, keystorePassword.toCharArray())
+                                    .loadTrustMaterial(new TrustSelfSignedStrategy())
+                                    .build();
+
+        } catch (GeneralSecurityException gse) {
+            log.error("", gse);
+            throw new IllegalArgumentException(gse);
+        } catch (IOException ie) {
+            log.error("", ie);
+            throw new UncheckedIOException(ie);
+        }
+
+        SSLConnectionSocketFactory socketFactory
+                = new SSLConnectionSocketFactory(Optional.ofNullable(sslContext).orElseThrow());
+
+        return HttpClients.custom()
+                          .setSSLSocketFactory(socketFactory)
+                          .setMaxConnTotal(100)
+                          .setMaxConnPerRoute(5)
+                          .build();
+    }
+
+    @Bean
+    HttpComponentsClientHttpRequestFactory requestFactory(final HttpClient httpClient) {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+
+        requestFactory.setReadTimeout(5_000);
+        requestFactory.setConnectTimeout(3_000);
+        requestFactory.setHttpClient(httpClient);
+
+        return requestFactory;
+    }
+
+    @Bean(name = "clientCertificateAuthenticationRestTemplate")
+    public RestTemplate clientCertificationRestTemplate(final HttpComponentsClientHttpRequestFactory requestFactory) {
+        return new RestTemplate(requestFactory);
     }
 
 }
