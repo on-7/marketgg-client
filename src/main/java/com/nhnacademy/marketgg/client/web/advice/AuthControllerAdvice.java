df14feff3863edbbbed88291fd6b35fa74643bc0
Dongyeol <eastheat10@gmail.com>
2022-07-29 14:13:39 +0900

    Feat: 예외 처리 템플릿 생성 #99

---
+++ src/main/java/com/nhnacademy/marketgg/client/web/advice/AuthControllerAdvice.java
@@ -0,0 +1,45 @@
+package com.nhnacademy.marketgg.client.web.advice;
+
+import com.nhnacademy.marketgg.client.exception.LoginFailException;
+import com.nhnacademy.marketgg.client.exception.LogoutException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 인증 관련 예외를 처리합니다.
+ */
+@Slf4j
+@Order(100)
+@ControllerAdvice
+public class AuthControllerAdvice {
+
+    /**
+     * 로그아웃 시 발생하는 예외를 처리합니다.
+     *
+     * @param e - 로그아웃 시 발생한 예외
+     * @return - 에러 페이지
+     */
+    @ExceptionHandler(LogoutException.class)
+    public ModelAndView logoutException(LogoutException e) {
+        ModelAndView mav = new ModelAndView();
+
+        return mav;
+    }
+
+    /**
+     * 로그인시 발생한 예외를 처리합니다. (401)
+     *
+     * @param e - 401 예외
+     * @return - 에러 페이지
+     */
+    @ExceptionHandler(LoginFailException.class)
+    public ModelAndView loginFailException(LoginFailException e) {
+        ModelAndView mav = new ModelAndView();
+
+        return mav;
+    }
+
+}
