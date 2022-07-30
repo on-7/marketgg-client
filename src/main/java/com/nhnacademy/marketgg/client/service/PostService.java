4b876b0244c69a77c82a129cd8f59f7e94e8d1d8
psw4887 <psw4887@naver.com>
2022-07-31 01:13:09 +0900

    Docs: Service JavaDoc 작성완료 #104

--- src/main/java/com/nhnacademy/marketgg/client/service/PostService.java
+++ src/main/java/com/nhnacademy/marketgg/client/service/PostService.java
@@ -8,18 +8,71 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
+/**
+ * 고객센터 게시글과 관련된 Service 입니다.
+ *
+ * @version 1.0.0
+ */
 public interface PostService {
 
+    /**
+     * 게시판 타입에 맞는 게시글을 등록할 수 있는 메소드입니다.
+     *
+     * @param postRequest - 등록할 게시글의 정보를 담은 객체입니다.
+     * @param type - 등록할 게시글의 게시판 타입입니다.
+     * @throws JsonProcessingException Json 컨텐츠를 처리할 때 발생하는 모든 문제에 대한 예외처리입니다.
+     * @since 1.0.0
+     */
     void createPost(final PostRequest postRequest, final String type) throws JsonProcessingException;
 
+    /**
+     * 게시판 타입에 맞는 게시글 목록을 조회할 수 있는 메소드입니다.
+     *
+     * @param pageable - 조회할 게시글 목록의 페이지 정보입니다.
+     * @param type - 조회할 게시글의 게시판 목록입니다.
+     * @return 게시판 타입에 맞는 게시글 목록들을 반환합니다.
+     * @throws JsonProcessingException Json 컨텐츠를 처리할 때 발생하는 모든 문제에 대한 예외처리입니다.
+     * @since 1.0.0
+     */
     List<PostResponse> retrievesPosts(final Pageable pageable, final String type) throws JsonProcessingException;
 
+    /**
+     * 지정한 게시글의 상세정보를 조회할 수 있는 메소드입니다.
+     *
+     * @param boardNo - 지정한 게시글의 식별번호입니다.
+     * @param type - 지정한 게시글의 게시판 타입입니다.
+     * @return 지정한 게시글의 상세정보를 반환합니다.
+     * @since 1.0.0
+     */
     PostResponse retrievePost(final Long boardNo, final String type);
 
+    /**
+     * 지정한 1:1 문의의 상세정보를 조회 할 수 있는 메소드입니다.
+     *
+     * @param boardNo - 지정한 1:1문의의 식별번호입니다.
+     * @param type - 지정한 1:1문의의 게시판 타입입니다.
+     * @return 지정한 1:1 문의의 상세정보를 반환합니다.
+     * @since 1.0.0
+     */
     PostResponseForOtoInquiry retrievePostForOtoInquiry(final Long boardNo, final String type);
 
+    /**
+     * 지정한 게시글을 수정할 수 있는 메소드입니다.
+     *
+     * @param boardNo - 수정할 게시글의 식별번호입니다.
+     * @param postRequest - 수정할 게시글의 정보를 담은 객체입니다.
+     * @param type - 수정할 게시글의 게시판 타입입니다.
+     * @since 1.0.0
+     */
     void updatePost(final Long boardNo, final PostRequest postRequest, final String type);
 
+    /**
+     * 지정한 게시글을 삭제할 수 있는 메소드입니다.
+     *
+     * @param boardNo - 삭제할 게시글의 식별번호입니다.
+     * @param type - 삭제할 게시글의 게시판 타입입니다.
+     * @since 1.0.0
+     */
     void deletePost(final Long boardNo, final String type);
 
 }
