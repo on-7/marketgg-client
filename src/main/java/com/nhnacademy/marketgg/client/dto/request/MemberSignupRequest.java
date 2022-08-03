c2b3b78003ba036ab473c88cf8fa65d1c558f1a9
Dongyeol <eastheat10@gmail.com>
2022-08-03 11:36:07 +0900

    Style: 코드 포멧팅

--- src/main/java/com/nhnacademy/marketgg/client/dto/request/MemberSignupRequest.java
+++ src/main/java/com/nhnacademy/marketgg/client/dto/request/MemberSignupRequest.java
@@ -2,8 +2,12 @@ package com.nhnacademy.marketgg.client.dto.request;
 
 import java.time.LocalDate;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @AllArgsConstructor
@@ -11,38 +15,38 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class MemberSignupRequest {
 
     @Email
-    // @NotBlank
+    @NotBlank
     private final String email;
 
     //'숫자', '문자', '특수문자' 무조건 1개 이상, 비밀번호 '최소 8자에서 최대 16자'까지 허용
     //(특수문자는 정의된 특수문자만 사용 가능)
-    // @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$")
     private String password;
 
-    // @NotBlank
+    @NotBlank
     private final String name;
 
-    // @NotBlank
+    @NotBlank
     private final String phoneNumber;
 
-    // @NotNull
+    @NotNull
     private final Character gender;
 
-    // @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private final LocalDate birthDate;
 
     private final String referrerEmail;
 
-    // @NotNull
+    @NotNull
     private final Integer zipcode;
 
-    // @NotBlank
+    @NotBlank
     private final String address;
 
-    // @NotBlank
+    @NotBlank
     private final String detailAddress;
 
-    // @NotNull
+    @NotNull
     private final Boolean inlineRadioOptions;
 
     private final String provider;
