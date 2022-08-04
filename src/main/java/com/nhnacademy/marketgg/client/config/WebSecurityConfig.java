b9678fb20733f6f69563d75c0cd1e768c93ff648
Dongyeol <eastheat10@gmail.com>
2022-08-04 11:28:55 +0900

    Hotfix: Multipart-form CSRF 토큰 이슈

--- src/main/java/com/nhnacademy/marketgg/client/config/WebSecurityConfig.java
+++ src/main/java/com/nhnacademy/marketgg/client/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.multipart.support.MultipartFilter;
 
 /**
  * Spring Security 기본 설정을 진행합니다.
