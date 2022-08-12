303beded8893a08080dcb452ad718235764e8af9
Dongyeol <eastheat10@gmail.com>
2022-08-12 11:04:33 +0900

    Feat: Security Context Holder 를 적용한 Interceptor 생성 #138

--- src/test/java/com/nhnacademy/marketgg/client/web/CartControllerTest.java
+++ src/test/java/com/nhnacademy/marketgg/client/web/CartControllerTest.java
@@ -29,6 +29,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -36,7 +37,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@WebMvcTest(CartController.class)
+// @WebMvcTest(CartController.class)
+@SpringBootTest
 class CartControllerTest {
 
     MockMvc mockMvc;
