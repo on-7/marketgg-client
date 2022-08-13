3b869c75b17f0f2a44aebbb1d5421861a1614aad
com-sun <gnlslfflffl@gmail.com>
2022-08-13 17:47:20 +0900

    Feat: 후기 조회 구현

--- src/main/java/com/nhnacademy/marketgg/client/web/product/ReviewController.java
+++ src/main/java/com/nhnacademy/marketgg/client/web/product/ReviewController.java
@@ -3,11 +3,14 @@ package com.nhnacademy.marketgg.client.web.product;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nhnacademy.marketgg.client.dto.MemberInfo;
 import com.nhnacademy.marketgg.client.dto.request.ReviewCreateRequest;
+import com.nhnacademy.marketgg.client.dto.response.ReviewResponse;
 import com.nhnacademy.marketgg.client.service.ReviewService;
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,5 +38,25 @@ public class ReviewController {
         reviewService.createReview(productId, reviewId, memberInfo, reviewRequest);
 
         return new ModelAndView("redirect:" + "/products/" + productId);
+    }
+
+    @GetMapping
+    public ModelAndView retrieveReviews(@PathVariable final Long productId,
+                                        final MemberInfo memberInfo) {
+
+        List<ReviewResponse> reviewResponses = reviewService.retrieveReviews(productId, memberInfo);
+
+        ModelAndView mav = new ModelAndView("/products/" + productId);
+        mav.addObject("reviews", reviewResponses);
+
+        return mav;
+    }
+
+    @GetMapping("/{reviewId}")
+    ModelAndView retrieveReview(@PathVariable final Long productId,
+                                @PathVariable final Long reviewId,
+                                final MemberInfo memberInfo) {
+
+        reviewService.retrieveReview(productId, reviewId, memberInfo);
     }
 }
