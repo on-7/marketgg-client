75825aa4c8246f37b87d4b2db95448219719d54d
com-sun <gnlslfflffl@gmail.com>
2022-08-14 20:07:45 +0900

    Docs: 자바독 작성

--- src/main/java/com/nhnacademy/marketgg/client/web/ReviewController.java
+++ src/main/java/com/nhnacademy/marketgg/client/web/ReviewController.java
@@ -21,6 +21,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 후기를 관리하기 위한 controller입니다.
+ *
+ * @author 조현진
+ */
 @Controller
 @RequestMapping("/products/{productId}/reviews")
 @RequiredArgsConstructor
@@ -28,6 +33,17 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
+    /**
+     * 후기를 등록합니다.
+     * 권한이 있는 사용자만 후기를 등록할 수 있습니다.
+     *
+     * @param productId     - 후기가 달릴 상품의 번호입니다.
+     * @param memberInfo    - 권한 체크를 위한 사용자 정보입니다.
+     * @param reviewRequest - 등록될 후기의 정보를 담고있습니다.
+     * @param bindingResult - validation 체크를 합니다.
+     * @return - 해당 상품의 view를 반환합니다.
+     * @throws JsonProcessingException - Json expeption을 던집니다.
+     */
     @RoleCheck
     @PostMapping
     public ModelAndView createReview(@PathVariable final Long productId,
@@ -44,6 +60,12 @@ public class ReviewController {
         return new ModelAndView("redirect:/" + "products/product-view");
     }
 
+    /**
+     * 후기의 전체 목록을 조회합니다.
+     *
+     * @param productId - 후기가 달린 상품의 상품번호입니다.
+     * @return - 후기 목록이 담긴 view를 반환합니다.
+     */
     @GetMapping
     public ModelAndView retrieveReviews(@PathVariable final Long productId) {
 
@@ -55,6 +77,13 @@ public class ReviewController {
         return mav;
     }
 
+    /**
+     * 선택한 후기를 상세조회합니다.
+     *
+     * @param productId - 해당 후기가 달린 상품의 상품번호입니다.
+     * @param reviewId  - 선택한 후기의 후기 번호입니다.
+     * @return - 후기의 상세 정보가 담긴 view를 반환합니다.
+     */
     @GetMapping("/{reviewId}")
     public ModelAndView retrieveReview(@PathVariable final Long productId,
                                        @PathVariable final Long reviewId) {
@@ -67,6 +96,18 @@ public class ReviewController {
         return mav;
     }
 
+    /**
+     * 선택한 후기를 수정합니다.
+     * 권한이 있는 사용자만 수정할 수 있습니다.
+     *
+     * @param productId     - 후기가 달린 상품의 상품번호입니다.
+     * @param reviewId      - 달린 후기의 후기번호입니다.
+     * @param memberInfo    - 후기를 수정할 회원의 회원정보입니다.
+     * @param reviewRequest - 수정할 후기의 정보다 담겨있습니다.
+     * @param bindingResult - validation체크를 합니다.
+     * @return - 후기가 달릴 상품의 view를 반환합니다.
+     * @throws JsonProcessingException - JsonException을 던집니다.
+     */
     @RoleCheck
     @PutMapping("/{reviewId}")
     public ModelAndView updateReview(@PathVariable final Long productId, @PathVariable final Long reviewId,
@@ -83,6 +124,14 @@ public class ReviewController {
         return new ModelAndView("redirect:/" + "products/product-view");
     }
 
+    /**
+     * 선택한 후기를 삭제합니다.
+     *
+     * @param productId  - 삭제할 후기가 달린 상품의 상품번호입니다.
+     * @param reviewId   - 삭제할 후기의 후기번호입니다.
+     * @param memberInfo - 본인이 작성한 후기인지 검증하기 위한 회원의 정보입니다.
+     * @return - 해당 상품의 view를 반환합니다.
+     */
     @RoleCheck
     @DeleteMapping("/{reviewId}")
     public ModelAndView deleteReview(@PathVariable final Long productId, @PathVariable final Long reviewId,
