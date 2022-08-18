4aec78e5a0b1604dc4f258f365d56f47481613d2
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-08-18 09:10:26 +0900

    Fix: 상수 변경 #152

--- src/main/java/com/nhnacademy/marketgg/client/config/WebSecurityConfig.java
+++ src/main/java/com/nhnacademy/marketgg/client/config/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig {
             .antMatchers("/admin/**").hasRole("ADMIN")
             // TODO: 로그인이 필요한 경로 추가 해야합니다.
             .antMatchers("/cart/**", "/dibs/**", "/members/dibs/**", "/members/ggpass/**",
-                         "/customer-services/categories/OTO_CODE/**").authenticated()
+                         "/customer-services/categories/" + OTO_CODE + "/**").authenticated()
             .anyRequest().permitAll();
 
         http.headers()
