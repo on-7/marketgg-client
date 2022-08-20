c1ffe0a9a69b24077f34be77a7ff58decf78a405
Dongyeol <eastheat10@gmail.com>
2022-08-21 02:54:27 +0900

    Feat: 로그인 페이지 디자인 #164

---
+++ src/main/java/com/nhnacademy/marketgg/client/config/InterceptorConfigure.java
@@ -0,0 +1,48 @@
+package com.nhnacademy.marketgg.client.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.marketgg.client.interceptor.AuthenticationInterceptor;
+import com.nhnacademy.marketgg.client.interceptor.CookieInterceptor;
+import com.nhnacademy.marketgg.client.jwt.JwtInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+/**
+ * Interceptor 추가를 위한 설정 클래스.
+ *
+ * @author 윤동열
+ */
+@Configuration
+@RequiredArgsConstructor
+public class InterceptorConfigure implements WebMvcConfigurer {
+
+    private final RedisTemplate<String, JwtInfo> redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CookieInterceptor())
+                .excludePathPatterns("/login", "/signup", "/plugins/**", "/css/**", "/images/**")
+                .order(-1);
+
+        registry.addInterceptor(new AuthenticationInterceptor(redisTemplate, objectMapper))
+                .excludePathPatterns("/login", "/signup", "/plugins/**", "/css/**", "/images/**")
+                .order(1);
+
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+}
