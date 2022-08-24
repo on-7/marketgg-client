02d6adc5807b1c5e5c93f7364394096dbc972f9c
cororing <corock.dev@gmail.com>
2022-08-24 18:42:28 +0900

    Fix: 가상계좌 DTO 추가 (hotfix)

---
+++ src/main/java/com/nhnacademy/marketgg/client/dto/payment/VirtualAccountDepositRequest.java
@@ -0,0 +1,42 @@
+package com.nhnacademy.marketgg.client.dto.payment;
+
+import java.time.LocalDateTime;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+/**
+ * 회원이 가상계좌에 주문한 건에 대해 입금한 정보를 포함하고 있는 클래스입니다.
+ *
+ * @author 이제훈
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@RequiredArgsConstructor
+@Getter
+@ToString
+public class VirtualAccountDepositRequest {
+
+    // @NotNull
+    // private final LocalDateTime createdAt;
+
+    @NotBlank
+    @Size(max = 50)
+    private final String secret;
+
+    @NotBlank
+    @Size(max = 20)
+    private final String status;
+
+    // @NotBlank
+    // @Size(max = 64)
+    // private final String transactionKey;
+
+    @NotBlank
+    @Size(min = 6, max = 64)
+    private final String orderId;
+
+}
