c3864509e8f63193195477bea5258f7b6d2e7e8a
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-08-24 09:13:34 +0900

    Fix: GG패스 삭제 #170

--- src/main/java/com/nhnacademy/marketgg/client/service/MemberService.java
+++ src/main/java/com/nhnacademy/marketgg/client/service/MemberService.java
@@ -27,37 +27,6 @@ public interface MemberService {
     EmailUseResponse useEmail(EmailRequest emailRequest);
 
     /**
-     * 회원의 GG Pass 갱신일시를 반환하는 메소드입니다.
-     *
-     * @return 회원의 갱신일시를 반환합니다.
-     * @throws UnAuthenticException     - 인증되지 않은 사용자가 접근 시 발생하는 예외입니다.
-     * @throws UnAuthorizationException - 권한이 없는 사용자가 접근 시 발생하는 예외입니다.
-     * @author 박세완
-     * @since 1.0.0
-     */
-    LocalDateTime retrievePassUpdatedAt() throws UnAuthenticException, UnAuthorizationException;
-
-    /**
-     * 회원의 GG Pass 구독처리를 위한 메소드입니다.
-     *
-     * @throws UnAuthenticException     - 인증되지 않은 사용자가 접근 시 발생하는 예외입니다.
-     * @throws UnAuthorizationException - 권한이 없는 사용자가 접근 시 발생하는 예외입니다.
-     * @author 박세완
-     * @since 1.0.0
-     */
-    void subscribePass() throws UnAuthenticException, UnAuthorizationException;
-
-    /**
-     * 회원의 GG Pass 구독해지처리를 위한 메소드입니다.
-     *
-     * @throws UnAuthenticException     - 인증되지 않은 사용자가 접근 시 발생하는 예외입니다.
-     * @throws UnAuthorizationException - 권한이 없는 사용자가 접근 시 발생하는 예외입니다.
-     * @author 박세완
-     * @since 1.0.0
-     */
-    void withdrawPass() throws UnAuthenticException, UnAuthorizationException;
-
-    /**
      * 지정한 회원의 정보 수정을 위한 메소드입니다.
      *
      * @param memberUpdateToAuth - 회원이 입력한 수정정보입니다.
--- src/test/java/com/nhnacademy/marketgg/client/service/MemberServiceTest.java
+++ src/test/java/com/nhnacademy/marketgg/client/service/MemberServiceTest.java
@@ -1,18 +1,7 @@
 package com.nhnacademy.marketgg.client.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.BDDMockito.willDoNothing;
-import static org.mockito.Mockito.times;
-
-import com.nhnacademy.marketgg.client.exception.auth.UnAuthenticException;
-import com.nhnacademy.marketgg.client.exception.auth.UnAuthorizationException;
 import com.nhnacademy.marketgg.client.repository.MemberRepository;
 import com.nhnacademy.marketgg.client.service.impl.DefaultMemberService;
-import java.time.LocalDateTime;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -26,35 +15,5 @@ class MemberServiceTest {
 
     @Mock
     private MemberRepository memberRepository;
-
-    @Test
-    @DisplayName("회원 GG 패스 갱신일 조회")
-    void retrievePassUpdatedAt() throws UnAuthenticException, UnAuthorizationException {
-        given(memberRepository.retrievePassUpdatedAt()).willReturn(LocalDateTime.now());
-
-        LocalDateTime memberUpdateDate = memberService.retrievePassUpdatedAt();
-
-        assertThat(memberUpdateDate.isAfter(LocalDateTime.now())).isFalse();
-    }
-
-    @Test
-    @DisplayName("회원 GG 패스 구독")
-    void subscribePass() throws UnAuthenticException, UnAuthorizationException {
-        willDoNothing().given(memberRepository).subscribePass();
-
-        memberService.subscribePass();
-
-        then(memberRepository).should(times(1)).subscribePass();
-    }
-
-    @Test
-    @DisplayName("회원 GG 패스 해지")
-    void withdrawPass() throws UnAuthenticException, UnAuthorizationException {
-        willDoNothing().given(memberRepository).withdrawPass();
-
-        memberService.withdrawPass();
-
-        then(memberRepository).should(times(1)).withdrawPass();
-    }
 
 }
