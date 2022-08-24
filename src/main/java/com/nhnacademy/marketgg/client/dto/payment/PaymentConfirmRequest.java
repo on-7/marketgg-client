e539f4709fa9fa6098e441fcc98d4d09f3c6a2be
cororing <corock.dev@gmail.com>
2022-08-24 18:40:05 +0900

    Fix: 조회 메서드 레이어별 추가 (hotfix)

--- src/main/java/com/nhnacademy/marketgg/client/dto/payment/PaymentConfirmRequest.java
+++ src/main/java/com/nhnacademy/marketgg/client/dto/payment/PaymentConfirmRequest.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 /**
  * 결제 승인 요청을 검증하기 위한 객체입니다.
@@ -13,20 +14,20 @@ import lombok.Getter;
  * @version 1.0
  * @since 1.0
  */
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public class PaymentConfirmRequest {
 
     @NotBlank
     @Size(min = 6, max = 64)
-    private String orderId;
+    private final String orderId;
 
     @NotBlank
     @Size(max = 200)
-    private String paymentKey;
+    private final String paymentKey;
 
     @NotNull
-    private Long amount;
+    private final Long amount;
 
     public static PaymentConfirmRequest create(final String orderId, final String paymentKey, final Long amount) {
         return new PaymentConfirmRequest(orderId, paymentKey, amount);
