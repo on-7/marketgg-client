a39dbd203bdf48c27255479fba1f75b8dd299473
com-sun <gnlslfflffl@gmail.com>
2022-08-25 20:08:57 +0900

    Fix: 상품 전체조회 URI 수정

--- src/main/java/com/nhnacademy/marketgg/client/web/IndexController.java
+++ src/main/java/com/nhnacademy/marketgg/client/web/IndexController.java
@@ -1,17 +1,32 @@
 package com.nhnacademy.marketgg.client.web;
 
 import com.nhnacademy.marketgg.client.dto.MemberInfo;
+import com.nhnacademy.marketgg.client.dto.PageResult;
+import com.nhnacademy.marketgg.client.dto.response.ImageResponse;
+import com.nhnacademy.marketgg.client.dto.response.ProductResponse;
+import com.nhnacademy.marketgg.client.paging.Pagination;
+import com.nhnacademy.marketgg.client.service.ImageService;
+import com.nhnacademy.marketgg.client.service.ProductService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final ProductService productService;
+    private final ImageService imageService;
+    private static final String DEFAULT_PRODUCT_VIEW = "products/product-view";
+
+
     @GetMapping({ "/", "/index" })
-    public ModelAndView index(MemberInfo memberInfo) {
+    public ModelAndView index(MemberInfo memberInfo, @RequestParam(defaultValue = "0") int page) {
         log.info("MemberInfo = {}", memberInfo);
 
         ModelAndView mav = new ModelAndView("index");
@@ -19,6 +34,18 @@ public class IndexController {
         if (!memberInfo.isNull()) {
             mav.addObject("memberInfo", memberInfo);
         }
+
+        PageResult<ProductResponse> productResponsePageResult = this.productService.retrieveProducts(page);
+        Pagination pagination = new Pagination(productResponsePageResult.getTotalPages(), page);
+        List<ProductResponse> products = productResponsePageResult.getData();
+
+        mav.addObject("products", products);
+
+        for (ProductResponse product : products) {
+            ImageResponse imageResponse = imageService.retrieveImage(product.getAssetNo());
+            product.updateThumbnail(imageResponse.getImageAddress());
+        }
+        mav.addObject("pages", pagination);
 
         return mav;
     }
