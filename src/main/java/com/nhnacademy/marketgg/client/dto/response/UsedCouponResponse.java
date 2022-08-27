432699c17dba5c7b0ade1a7d26a21c5619e9f7e7
Moomin <moominK228@gmail.com>
2022-08-28 00:07:47 +0900

    Feat: 주문 상세 조회에 사용한 쿠폰 내용 추가 #190

---
+++ src/main/java/com/nhnacademy/marketgg/client/dto/response/UsedCouponResponse.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.marketgg.client.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class UsedCouponResponse {
+
+    private String name;
+
+    private Double discountAmount;
+
+}
