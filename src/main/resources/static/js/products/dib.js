4ffc4c4773276d69543f2c9152da01c1fc6441ce
com-sun <gnlslfflffl@gmail.com>
2022-08-28 15:13:00 +0900

    Fix: 찜 오류 수정

--- src/main/resources/static/js/products/dib.js
+++ src/main/resources/static/js/products/dib.js
@@ -1,55 +1,57 @@
 window.addEventListener('DOMContentLoaded', () => {
-  let productId = 19;
+    const productId = document.getElementById("product-id").value;
+    let csrfToken = document.getElementById("_csrf").content;
 
-  let csrfToken = document.getElementById("_csrf").content;
+    function dibView() {
 
-  function dibView() {
-    $.ajax({
-      // TODO: 올릴때 주소 바꾸기
-      url: 'http://localhost:5050/dibs',
-      type: 'POST',
-      headers: { "Content-Type": "application/json", "X-CSRF-TOKEN": csrfToken },
-      async: true,
-      data: {
-        'productId': productId
-      },
-      dataType: "json",
-      success: function (data) {
-        let htmlBody = '';
-        if (data.exist === 1) {
-          htmlBody += '<button onclick="dibDelete(productId);"'
-            + '><span>찜제거</span></button>'
-        } else {
-          htmlBody += '<button onclick="dibInsert(productId);"'
-            + '><span>찜</span></button>'
-        }
-        $('#dib').html(htmlBody);
-      }
-    })
-  }
+        $.ajax({
+            url: 'http://localhost:5050/dibs',
+            type: 'POST',
+            headers: {"Content-Type": "application/json", "X-CSRF-TOKEN": csrfToken},
+            async: true,
+            data: {
+                'productId': productId
+            },
+            dataType: "json",
+            success: function (data) {
+                let htmlBody = '';
+                if (data.exist === 1) {
+                    htmlBody += '<button class="btn btn-success" onclick="dibDelete(productId);"'
+                        + '><span>찜제거</span></button>'
+                } else {
+                    htmlBody += '<button class="btn btn-success" onclick="dibInsert(productId);"'
+                        + '><span>찜</span></button>'
+                }
+                $('#dib').html(htmlBody);
+                console.log(productId);
+            }
+        })
+    }
 
-  function dibInsert(productId) {
-    $.ajax({
-      url: '/dibs/insert/' + productId,
-      type: 'get',
-      success: function (data) {
-        if (data === 1)
-          dibView();
-      }
-    });
-  }
-
-  function dibDelete(productId) {
-    $.ajax({
-      url: '/dibs/delete/' + productId,
-      type: 'get',
-      success: function (data) {
-        if (data === 1)
-          dibView();
-      }
-    });
-  }
-
-  dibView();
+    dibView();
 
 });
+
+function dibInsert(productId) {
+    $.ajax({
+        url: 'http://localhost:5050/dibs/insert/' + productId,
+        type: 'get',
+        success: function (data) {
+            if (data === 1)
+                dibView();
+        }
+    });
+    console.log("success");
+}
+
+function dibDelete(productId) {
+    $.ajax({
+        url: 'http://localhost:5050/dibs/delete/' + productId,
+        type: 'get',
+        success: function (data) {
+            if (data === 1)
+                dibView();
+        }
+    });
+    console.log("success");
+}
