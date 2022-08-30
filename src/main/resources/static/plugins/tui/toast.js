be1bed55d607861dfc2057ade6d72ad3339121e8
Moomin <moominK228@gmail.com>
2022-08-30 20:57:33 +0900

    Fix: toast ui auto focus 해결

--- src/main/resources/static/plugins/tui/toast.js
+++ src/main/resources/static/plugins/tui/toast.js
@@ -1,53 +1,55 @@
-const Toast = toastui.Editor;
-let csrfToken = document.getElementById("_csrf").content;
+document.addEventListener('DOMContentLoaded', () => {
+    const Toast = toastui.Editor;
+    let csrfToken = document.getElementById("_csrf").content;
 
-const editor = new Toast({
-    el: document.querySelector('#editor'),
-    height: 'auto',
-    initialEditType: 'wysiwyg',
-    previewStyle: 'vertical',
-    placeholder: '내용을 입력해주세요.',
-    hooks: {
-        addImageBlobHook: (blob, callback) => {
-            console.log(blob);
+    const editor = new Toast({
+        el: document.querySelector('#editor'),
+        height: 'auto',
+        initialEditType: 'wysiwyg',
+        previewStyle: 'vertical',
+        placeholder: '내용을 입력해주세요.',
+        hooks: {
+            addImageBlobHook: (blob, callback) => {
+                console.log(blob);
 
-            const formData = new FormData();
-            formData.append('image', blob);
+                const formData = new FormData();
+                formData.append('image', blob);
 
-            let url = '';
-            $.ajax({
-                type: 'POST',
-                enctype: 'multipart/form-data',
-                url: '/editor',
-                data: formData,
-                dataType: 'text',
-                processData: false,
-                contentType: false,
-                cache: false,
-                timeout: 600000,
-                success: function (data) {
-                    console.log("ajax 성공");
-                    url += data;
-                    callback(url);
-                    console.log(url);
-                },
-                error: function (e) {
-                    console.log("ajax 업로드 실패!")
-                    console.log(e.abort([statusText]));
-                    callback('image_load_fail', '실패용');
-                    console.log(url);
-                    console.log(e)
-                }
-            });
-        }
-    }
+                let url = '';
+                $.ajax({
+                    type: 'POST',
+                    enctype: 'multipart/form-data',
+                    url: '/editor',
+                    data: formData,
+                    dataType: 'text',
+                    processData: false,
+                    contentType: false,
+                    cache: false,
+                    timeout: 600000,
+                    success: function (data) {
+                        console.log("ajax 성공");
+                        url += data;
+                        callback(url);
+                        console.log(url);
+                    },
+                    error: function (e) {
+                        console.log("ajax 업로드 실패!")
+                        console.log(e.abort([statusText]));
+                        callback('image_load_fail', '실패용');
+                        console.log(url);
+                        console.log(e)
+                    }
+                });
+            }
+        },
+        autofocus: false,
+    });
+
+    const submitButton = document.getElementById('btn-submit');
+
+    submitButton.addEventListener('click', (event) => {
+        const description = editor.getHTML();
+        console.log(description);
+        document.getElementById('toast-content').value = description;
+    });
 });
-
-const submitButton = document.getElementById('btn-submit');
-
-submitButton.addEventListener('click', (event) => {
-    const description = editor.getHTML();
-    console.log(description);
-    document.getElementById('toast-content').value = description;
-})
-
