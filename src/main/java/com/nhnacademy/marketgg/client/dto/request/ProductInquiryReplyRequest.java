71ef03354ee7e9535974f106aeb64aa20fbf8977
coalong <minay0904@gmail.com>
2022-08-30 13:17:16 +0900

    Refactor: 관리자 문의 답글 Dto 수정

--- src/main/java/com/nhnacademy/marketgg/client/dto/request/ProductInquiryReplyRequest.java
+++ src/main/java/com/nhnacademy/marketgg/client/dto/request/ProductInquiryReplyRequest.java
@@ -10,12 +10,6 @@ import lombok.Getter;
 public class ProductInquiryReplyRequest {
 
     @NotNull
-    private Long productId;
-
-    @NotNull
-    private Long inquiryId;
-
-    @NotNull
     @Size(min = 10, max = 300, message = "문의 답변 내용은 10자 이상, 300자 이하만 가능합니다.")
     private String adminReply;
 
