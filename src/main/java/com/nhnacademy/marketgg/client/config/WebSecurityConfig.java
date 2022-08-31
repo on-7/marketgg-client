3e16b838e836b3f796fa7b06996c0f41e36e1078
cororing <corock.dev@gmail.com>
2022-08-31 11:33:46 +0900

    Feat: 마이 GG 페이지 레이아웃 작성 #233

--- src/main/java/com/nhnacademy/marketgg/client/config/WebSecurityConfig.java
+++ src/main/java/com/nhnacademy/marketgg/client/config/WebSecurityConfig.java
@@ -46,7 +46,7 @@ public class WebSecurityConfig {
             .antMatchers("/admin/**").hasRole("ADMIN")
             // TODO: 로그인이 필요한 경로 추가 해야합니다.
             .antMatchers("/cart/**", "/dibs/**", "/members/dibs/**", "/members/ggpass/**",
-                         "/customer-services/categories/" + OTO_CODE + "/**", "/orders/**").authenticated()
+                         "/customer-services/categories/" + OTO_CODE + "/**", "/orders/**", "/mygg/**").authenticated()
             .anyRequest().permitAll();
 
         http.headers()
