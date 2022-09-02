6c29ffc0b142750c35e284e239150b38a7fb4b6e
com-sun <gnlslfflffl@gmail.com>
2022-09-02 15:01:12 +0900

    Fix: typo

--- src/main/java/com/nhnacademy/marketgg/client/web/product/ProductController.java
+++ src/main/java/com/nhnacademy/marketgg/client/web/product/ProductController.java
@@ -137,7 +137,7 @@ public class ProductController {
      */
     @GetMapping("/categories/{categoryCode}")
     public ModelAndView retrieveProductsByCategory(@PathVariable final String categoryCode,
-                                                   @RequestParam(defaultValue = "0") int page) {
+                                                   @RequestParam(defaultValue = "1") int page) {
 
         PageResult<ProductListResponse> searchProductResponsePageResult =
                 this.productService.retrieveProductsByCategory(
