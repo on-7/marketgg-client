450b7970dcb06cd53882da6e69d2fa5ef96c29e5
Moomin <moominK228@gmail.com>
2022-09-05 10:50:48 +0900

    Feat: 로그인해야 후기등록, 문의등록 가능 #245

--- src/main/java/com/nhnacademy/marketgg/client/config/WebSecurityConfig.java
+++ src/main/java/com/nhnacademy/marketgg/client/config/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig {
             .antMatchers("/admin/**").hasRole("ADMIN")
             // TODO: 로그인이 필요한 경로 추가 해야합니다.
             .antMatchers("/cart/**", "/dibs/**", "/members/dibs/**", "/members/ggpass/**",
-                "/customer-services/categories/" + OTO_CODE + "/**", "/orders/**", "/mygg/**").authenticated()
+                "/customer-services/categories/" + OTO_CODE + "/**", "/orders/**", "/mygg/**", "/**/inquiries").authenticated()
             .anyRequest().permitAll();
 
         http.headers()
