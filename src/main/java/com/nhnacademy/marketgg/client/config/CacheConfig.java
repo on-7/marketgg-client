b6d982710b47df6bc827ce70b2b43c9c3598caff
com-sun <gnlslfflffl@gmail.com>
2022-09-05 17:14:36 +0900

    Feat: EhCacheManager 등록

--- src/main/java/com/nhnacademy/marketgg/client/config/CacheConfig.java
+++ src/main/java/com/nhnacademy/marketgg/client/config/CacheConfig.java
@@ -2,22 +2,34 @@ package com.nhnacademy.marketgg.client.config;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
+import org.springframework.core.io.ClassPathResource;
 
 @EnableCaching
 @Configuration
 public class CacheConfig {
 
+//    @Bean
+//    public CacheManager cacheManager() {
+//        SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
+//        simpleCacheManager.setCaches(List.of(new ConcurrentMapCache("category")));
+//        return simpleCacheManager;
+//    }
+//
+
     @Bean
     public CacheManager cacheManager() {
-        SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
-        simpleCacheManager.setCaches(List.of(new ConcurrentMapCache("category")));
-        //EH 캐시. 심플은 기능이 너무 단순하다.기능이 없다.. EH 캐시를 대부분 사용한다.
-        return simpleCacheManager;
+        return new EhCacheCacheManager(ehCacheManager().getObject());
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheManager() {
+        EhCacheManagerFactoryBean factory = new EhCacheManagerFactoryBean();
+        factory.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        factory.setShared(true);
+        return factory;
     }
 }
